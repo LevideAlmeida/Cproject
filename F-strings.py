8d8408bf169a2fb3d5d992fed77d65fab000a116
Levi <thegood878@gmail.com>
2023-01-02 19:50:54 -0300

    Formatação de strings e seus tipos

---
+++ F-strings.py
@@ -0,0 +1,11 @@
+Nome = "Levi Almeida"
+altura = 1.75
+peso = 60
+imc = 60 / 1.75 ** 2
+
+linha_1 = f"{Nome} tem {altura:.2f} de altura"
+linha_2 = f"Pesa {peso} quilos e seu IMC é"
+
+print(linha_1)
+print(linha_2)
+print(f"{imc:.2f}")
---
+++ Formatar_com_Format.py
@@ -0,0 +1,6 @@
+a = 'AAAAAA'
+b = 'B'
+c = 2.2
+formato = 'a={0} b={1} a={0} c={nome1:.2f}'.format(a, b, nome1=c)
+
+print(formato)
