501c0c80845318b65b56f6b654b6ae8a6466bb56
Levi <thegood878@gmail.com>
2023-01-03 18:57:15 -0300

    Exercicio_if.py

---
+++ Exercicio_if.py
@@ -0,0 +1,9 @@
+valor_1 = input('Digite um valor: ')
+valor_2 = input('Digite outro valor: ')
+
+if valor_1 > valor_2:
+    print(f"{valor_1=} é maior que {valor_2=}")
+elif valor_1 < valor_2:
+    print(f"{valor_2=} é maior que {valor_1=}")
+elif valor_1 == valor_2:
+    print(f"{valor_1=} é igual a {valor_2=}")
