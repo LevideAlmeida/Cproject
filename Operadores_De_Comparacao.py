e5f8f83d9fcb69ba13beb78bfb95b0a780e06c80
Levi <thegood878@gmail.com>
2023-01-03 18:46:36 -0300

    Trabalhando com operadores de comparacao

---
+++ Operadores_De_Comparacao.py
@@ -0,0 +1,24 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo(true)
+>       Maior               5 > 3
+<       Menor               3 < 5
+>=      Maior ou igual      5 >= 5
+<=      Menor ou igual      2 <= 6
+==      Igual               'a' == 'a'
+!=      Diferente           'a' != 'b'
+"""
+
+maior = 5 > 3
+menor = 3 < 5
+maior_ou_igual = 5 >= 5
+menor_ou_igual = 2 <= 6
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+print(maior)
+print(menor)
+print(maior_ou_igual)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
