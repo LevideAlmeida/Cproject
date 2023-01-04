61c21fb827803d3e6c7a5a5ee074818508ec832f
Levi <thegood878@gmail.com>
2023-01-04 18:53:01 -0300

    ADD: Fatiamento_De_Strings.py

---
+++ Fatiamento_De_Strings.py
@@ -0,0 +1,18 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[2])
+print(variavel[-7])
+print(variavel[4:])
+print(variavel[:-3])
+print(variavel[2:-3])
+print(len(variavel))
+print(variavel[0:9:4])
+print(variavel[::-1])
