4ab1c8d425a1811773904d8ca3257085e51e9172
Levi <thegood878@gmail.com>
2023-01-31 19:09:18 -0300

    Conhecendo o For e o Range

---
+++ For.py
@@ -0,0 +1,9 @@
+texto = "Levi"
+
+novo_texto = ''
+
+for letra in texto:
+    print(letra)
+    novo_texto += f"*{letra}"
+
+print(novo_texto + "*")
---
+++ Range.py
@@ -0,0 +1,23 @@
+"""
+For + range
+range => range(start, stop, step)
+"""
+
+numeros = range(10)
+
+for numero in numeros:
+    print(numero)
+
+print("###########################")
+
+numeros = range(5, 10)
+
+for numero in numeros:
+    print(numero)
+
+print("###########################")
+
+numeros = range(4, 11, 2)
+
+for numero in numeros:
+    print(numero)
