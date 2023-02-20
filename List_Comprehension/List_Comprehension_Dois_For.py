26e57f3fe0ff1ab5a6ce6d9e08b3d0484224d4de
Levi <thegood878@gmail.com>
2023-02-20 19:06:49 -0300

    organizando os arquivos em pastas

---
+++ List_Comprehension/List_Comprehension_Dois_For.py
@@ -0,0 +1,21 @@
+lista = []
+for x in range(3):
+    for y in range(3):
+        lista.append((x,y))
+
+print(lista)
+
+lista = [
+    (x,y)
+    for x in range(3)
+    for y in range(3)
+]
+
+print(lista)
+
+lista = [
+    [x for x in range(3)]
+    for x in range(3)
+]
+
+print(lista)
