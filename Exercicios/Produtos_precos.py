67a86246306531550ed3088faf620e3955145ea9
Levi <thegood878@gmail.com>
2023-02-28 20:02:44 -0300

    exercises with list and dict

--- Exercicios/Produtos_precos.py
+++ Exercicios/Produtos_precos.py
@@ -16,8 +16,8 @@ products = [
 new_products = copy.deepcopy(products)
 
 for product in new_products:
-    ten_percent_product = round(0.1 * product['preco'], 1)
-    product['preco'] += ten_percent_product
+    ten_percent_product = round(0.1 * product['price'], 1)
+    product['price'] += ten_percent_product
 
 print(*new_products, sep='\n')
 
@@ -27,7 +27,7 @@ print(separator)
 # Generate products_sorted_by_name by deep copy
 
 products_sorted_by_name = copy.deepcopy(new_products)
-products_sorted_by_name.sort(reverse= True, key=lambda produto: produto['nome'])
+products_sorted_by_name.sort(reverse= True, key=lambda produto: produto['name'])
 print(*products_sorted_by_name, sep='\n')
 
 print(separator)
@@ -36,5 +36,5 @@ print(separator)
 # Generate products_sorted_by_price by deep copy
 
 products_sorted_by_price = copy.deepcopy(new_products)
-products_sorted_by_price.sort(key=lambda product: product['preco'])
+products_sorted_by_price.sort(key=lambda product: product['price'])
 print(*products_sorted_by_price, sep='\n')
